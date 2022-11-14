2c3049bce43b67bc78cebfbe0811676c84dad6cb
Charmull <527wlals@gmail.com>
2022-11-14 21:41:21 +0900

    feat: offer 메세지 받으면 해당 workspace의 다른 멤버들에게 offer 보내기

--- app.js
+++ app.js
@@ -15,29 +15,51 @@ app.get("/*", function (req, res) {
 });
 
 const workspaces = new Map();
+const sockets = new Map();
 
 wsServer.on("connection", (socket, req) => {
   console.log("socket connected");
   socket.on("message", (msg) => {
     const parsedMsg = JSON.parse(msg);
-    console.log(parsedMsg);
     console.log(wsServer.clients.size);
+    console.log(workspaces);
 
-    // socket의 workspace id
-    const workspaceId = parsedMsg.data;
-    // workspaceId에 맞춰 workspaces에 socket 추가
-    if (workspaces.get(workspaceId)) {
-      workspaces.get(workspaceId).push(socket);
-    } else {
-      workspaces.set(workspaceId, [socket]);
-    }
+    switch (parsedMsg.event) {
+      // socket 연결 (workspace 입장 시)
+      case "enter":
+        // socket의 workspace id
+        const workspaceId = parsedMsg.data;
+        // workspaceId에 맞춰 workspaces에 socket 추가
+        if (workspaces.get(workspaceId)) {
+          workspaces.get(workspaceId).push(socket);
+        } else {
+          workspaces.set(workspaceId, [socket]);
+        }
+        sockets.set(socket, workspaceId);
 
-    // socket 연결 시, 해당 socket과 연결된 client socket이 위치한 workspace를 찾고,
-    // workspace에 있는 멤버들에게 입장 사실을 socket message를 통해 알림
-    for (let memSocket of workspaces.get(workspaceId)) {
-      if (memSocket != socket) {
-        memSocket.send(JSON.stringify({ event: "enter", data: "workspace01" }));
-      }
+        // socket 연결 시, 해당 socket과 연결된 client socket이 위치한 workspace를 찾고,
+        // workspace에 있는 멤버들에게 입장 사실을 socket message를 통해 알림
+        for (let memSocket of workspaces.get(workspaceId)) {
+          if (memSocket != socket) {
+            memSocket.send(
+              JSON.stringify({ event: "enter", data: "workspace01" })
+            );
+          }
+        }
+        break;
+      // client에서 offer 생성 후 서버에서 offer msg 받을 시
+      case "offer":
+        // 해당 workspace에 있는 다른 멤버들에게 offer 보내기
+        // TODO: 3번째 멤버 입장부터, 이미 offer를 받아서 remoteOffer를 설정한 socket에도 offer를 다시 보내고 있음. 추후 재전송하지 않도록 수정 필요
+        const workspace = sockets.get(socket);
+        for (let memSocket of workspaces.get(workspace)) {
+          if (memSocket != socket) {
+            memSocket.send(
+              JSON.stringify({ event: "offer", data: parsedMsg.data })
+            );
+          }
+        }
+        break;
     }
   });
 });
