1f561f8dd60a5bd761d64d3845cdc8d0c50e8c1c
Charmull <527wlals@gmail.com>
2022-11-14 21:08:35 +0900

    feat: 멤버 입장 시 해당 룸에 메세지 생성
    - rooms를 통해 생성된 socket을 workspace별로 관리
    - 새로운 socket 생성 시 해당 workspace에 있는 멤버들에게 socket message 보냄 (멤버 입장 메세지)

--- app.js
+++ app.js
@@ -14,11 +14,31 @@ app.get("/*", function (req, res) {
   res.redirect("/");
 });
 
-wsServer.on("connection", (socket) => {
+const workspaces = new Map();
+
+wsServer.on("connection", (socket, req) => {
   console.log("socket connected");
   socket.on("message", (msg) => {
     const parsedMsg = JSON.parse(msg);
     console.log(parsedMsg);
+    console.log(wsServer.clients.size);
+
+    // socket의 workspace id
+    const workspaceId = parsedMsg.data;
+    // workspaceId에 맞춰 workspaces에 socket 추가
+    if (workspaces.get(workspaceId)) {
+      workspaces.get(workspaceId).push(socket);
+    } else {
+      workspaces.set(workspaceId, [socket]);
+    }
+
+    // socket 연결 시, 해당 socket과 연결된 client socket이 위치한 workspace를 찾고,
+    // workspace에 있는 멤버들에게 입장 사실을 socket message를 통해 알림
+    for (let memSocket of workspaces.get(workspaceId)) {
+      if (memSocket != socket) {
+        memSocket.send(JSON.stringify({ event: "enter", data: "workspace01" }));
+      }
+    }
   });
 });
 
