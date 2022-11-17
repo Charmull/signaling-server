4a68ca7972e1ed54c151fd63e20da9ad36eb8bde
Charmull <527wlals@gmail.com>
2022-11-17 17:52:37 +0900

    feat: signalingServer 코드 구조 변경
    - 3인 이상 1:1 connection 가능하도록 구조 변경
    - TODO: 가까운 시일 내에 코드 정리 및 리펙토링 필요

--- app.js
+++ app.js
@@ -22,11 +22,13 @@ wsServer.on("connection", (socket, req) => {
   socket.on("message", (msg) => {
     const parsedMsg = JSON.parse(msg);
     console.log(wsServer.clients.size);
-    console.log(workspaces);
+    // console.log(workspaces);
 
     switch (parsedMsg.event) {
       // socket 연결 (workspace 입장 시)
       case "enter":
+        socket["id"] = parsedMsg.from;
+        console.log(socket["id"]);
         // socket의 workspace id
         const workspaceId = parsedMsg.data;
         // workspaceId에 맞춰 workspaces에 socket 추가
@@ -37,37 +39,55 @@ wsServer.on("connection", (socket, req) => {
         }
         sockets.set(socket, workspaceId);
 
+        const memList = [];
         // socket 연결 시, 해당 socket과 연결된 client socket이 위치한 workspace를 찾고,
-        // workspace에 있는 멤버들에게 입장 사실을 socket message를 통해 알림
         for (let memSocket of workspaces.get(workspaceId)) {
-          if (memSocket != socket) {
+          // workspace에 있는 멤버들에게 입장 사실을 socket message를 통해 알림
+          if (memSocket !== socket) {
+            memList.push(memSocket["id"]);
             memSocket.send(
-              JSON.stringify({ event: "enter", data: "workspace01" })
+              JSON.stringify({
+                event: "memberEnter",
+                data: workspaceId,
+                from: socket["id"],
+              })
             );
           }
+        }
+        // 입장 시 본인 외 다른 사람이 있으면 다른 멤버들의 id 리스트를 보냄
+        if (memList.length != 0) {
+          socket.send(JSON.stringify({ event: "memList", data: memList }));
         }
         break;
       // client에서 offer 생성 후 서버에서 offer msg 받을 시
       case "offer":
         // 해당 workspace에 있는 다른 멤버들에게 offer 보내기
-        // TODO: 3번째 멤버 입장부터, 이미 offer를 받아서 remoteOffer를 설정한 socket에도 offer를 다시 보내고 있음. 추후 재전송하지 않도록 수정 필요
-        const workspace = sockets.get(socket);
-        for (let memSocket of workspaces.get(workspace)) {
-          if (memSocket != socket) {
+        const offerWS = sockets.get(socket);
+        for (let memSocket of workspaces.get(offerWS)) {
+          if (memSocket["id"] === parsedMsg.to) {
             memSocket.send(
-              JSON.stringify({ event: "offer", data: parsedMsg.data })
+              JSON.stringify({
+                event: "offer",
+                data: parsedMsg.data,
+                from: socket["id"],
+                to: memSocket["id"],
+              })
             );
           }
         }
         break;
       case "answer":
         // 해당 workspace에 있는 다른 멤버들에게 answer 보내기
-        // TODO: 3번째 멤버 입장부터, 이미 answer를 받아서 remoteOffer를 설정한 socket에도 answer를 다시 보내고 있음. 추후 재전송하지 않도록 수정 필요
-        const answerWs = sockets.get(socket);
-        for (let memSocket of workspaces.get(answerWs)) {
-          if (memSocket != socket) {
+        const answerWS = sockets.get(socket);
+        for (let memSocket of workspaces.get(answerWS)) {
+          if (memSocket["id"] === parsedMsg.to) {
             memSocket.send(
-              JSON.stringify({ event: "answer", data: parsedMsg.data })
+              JSON.stringify({
+                event: "answer",
+                data: parsedMsg.data,
+                from: socket["id"],
+                to: memSocket["id"],
+              })
             );
           }
         }
@@ -75,17 +95,57 @@ wsServer.on("connection", (socket, req) => {
       case "ice":
         // 해당 workspace에 있는 다른 멤버들에게 ice 보내기
         // TODO: 3번째 멤버 입장부터, 이미 answer를 받아서 remoteOffer를 설정한 socket에도 answer를 다시 보내고 있음. 추후 재전송하지 않도록 수정 필요
-        const iceWs = sockets.get(socket);
-        for (let memSocket of workspaces.get(iceWs)) {
-          if (memSocket != socket) {
+        const iceWS = sockets.get(socket);
+        for (let memSocket of workspaces.get(iceWS)) {
+          if (memSocket["id"] === parsedMsg.to) {
             memSocket.send(
-              JSON.stringify({ event: "ice", data: parsedMsg.data })
+              JSON.stringify({
+                event: "ice",
+                data: parsedMsg.data,
+                from: socket["id"],
+                to: memSocket["id"],
+              })
             );
           }
         }
         break;
+      case "exitPage":
+        console.log("Exit!!!!");
+        const exitPageWS = sockets.get(socket);
+        for (let memSocket of workspaces.get(exitPageWS)) {
+          if (memSocket !== socket) {
+            memSocket.send(
+              JSON.stringify({
+                event: "memberExit",
+                data: "exit",
+                from: socket["id"],
+                to: memSocket["id"],
+              })
+            );
+          }
+        }
+        workspaces
+          .get(exitPageWS)
+          .splice(workspaces.get(exitPageWS).indexOf(socket), 1);
+        sockets.delete(socket);
+        break;
     }
   });
+  // socket.on("close", () => {
+  //   const exitPageWS = sockets.get(socket);
+  //   for (let memSocket of workspaces.get(exitPageWS)) {
+  //     if (memSocket !== socket) {
+  //       memSocket.send(
+  //         JSON.stringify({
+  //           event: "memberExit",
+  //           data: "exit",
+  //           from: socket["id"],
+  //           to: memSocket["id"],
+  //         })
+  //       );
+  //     }
+  //   }
+  // });
 });
 
 httpServer.listen(3050, () => {
